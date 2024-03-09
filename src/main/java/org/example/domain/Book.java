fcbc40444d8eb57b20336fbc099d17969d3eb83f
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-09 11:46:37 +0900

    BookのIDにガード条件を追加

--- src/main/java/org/example/domain/Book.java
+++ src/main/java/org/example/domain/Book.java
@@ -7,7 +7,7 @@ import static org.apache.commons.lang3.StringUtils.isNumeric;
  * 本の情報を表す Value Object.
  *
  * @param id 本のid. nullであってはなりません.
- * @param title
+ * @param title 本のタイトル名.
  * @param author
  * @param publisher
  * @param price
@@ -18,7 +18,7 @@ public record Book(String id, String title, String author, String publisher, Int
    * 本の Value Object を初期化します.
    *
    * @param id 本のid. nullであってはなりません.
-   * @param title
+   * @param title 本のタイトル.
    * @param author
    * @param publisher
    * @param price
@@ -27,8 +27,13 @@ public record Book(String id, String title, String author, String publisher, Int
     if (isNull(id)) {
       throw new IllegalArgumentException("本のIDがnullです.");
     }
+
     if (!isNumeric(id)) {
       throw new IllegalArgumentException("本のIDが数字ではありません.");
+    }
+
+    if (Long.parseLong(id) <= 0L || Long.parseLong(id) > 9999999999L) {
+      throw new IllegalArgumentException("IDは1~9999999999でなくてはなりません.");
     }
   }
 }
