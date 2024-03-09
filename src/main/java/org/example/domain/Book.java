be34c5d7e84cfd4a2d310882a3388d69df07a301
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-09 10:08:49 +0900

    Bookのidを実装、pomを編集

--- src/main/java/org/example/domain/Book.java
+++ src/main/java/org/example/domain/Book.java
@@ -1,14 +1,24 @@
 package org.example.domain;
 
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 /**
  * 本の情報を表す Value Object.
  *
- * @param id
+ * @param id 本のid. nullであってはなりません.
  * @param title
  * @param author
  * @param publisher
  * @param price
  */
 public record Book(String id, String title, String author, String publisher, Integer price) {
+  public Book {
+    checkIdIsNull();
+  }
 
+  private void checkIdIsNull() {
+    if (!isNumeric(id)) {
+      throw new IllegalArgumentException("本のidがnullです.");
+    }
+  }
 }
