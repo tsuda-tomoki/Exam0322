82afb51aefad9305e8078c9606cccd8a4572fb08
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-09 14:07:25 +0900

    BookResponseの実装

--- src/main/java/org/example/presentation/response/BookResponse.java
+++ src/main/java/org/example/presentation/response/BookResponse.java
@@ -1,5 +1,32 @@
 package org.example.presentation.response;
 
-public record BookResponse(String id, String title, String author, String publisher, Integer price) {
+import org.example.domain.Book;
 
+/**
+ * 本の情報を表すレスポンスオブジェクト.
+ *
+ * @param id        本のID
+ * @param title     本のタイトル
+ * @param author    本の著者
+ * @param publisher 本の出版社
+ * @param price     本の金額
+ */
+public record BookResponse(
+    String id, String title, String author, String publisher, Integer price) {
+
+  /**
+   * Bookオブジェクトをレスポンスオブジェクトに変換します.
+   *
+   * @param book 本の情報を表す値オブジェクト
+   * @return 変換されたレスポンスオブジェクト
+   */
+  public static BookResponse format(Book book) {
+    return new BookResponse(
+        book.id(),
+        book.title(),
+        book.author(),
+        book.publisher(),
+        book.price()
+    );
+  }
 }
