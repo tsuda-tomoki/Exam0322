02aa56bea39a8d00f346c12ffce3524525c670d6
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-09 12:26:13 +0900

    Bookにタイトルのガード条件を追加

--- src/main/java/org/example/domain/Book.java
+++ src/main/java/org/example/domain/Book.java
@@ -6,19 +6,19 @@ import static org.apache.commons.lang3.StringUtils.isNumeric;
 /**
  * 本の情報を表す Value Object.
  *
- * @param id 本のid. nullであってはなりません.
- * @param title 本のタイトル名.
- * @param author
- * @param publisher
- * @param price
+ * @param id        本のID
+ * @param title     本のタイトル名
+ * @param author    本の著者
+ * @param publisher 本の出版社
+ * @param price     本の金額
  */
 public record Book(String id, String title, String author, String publisher, Integer price) {
 
   /**
    * 本の Value Object を初期化します.
    *
-   * @param id 本のid. nullであってはなりません.
-   * @param title 本のタイトル.
+   * @param id        本のID. 1~9999999999の数字でかつ, nullであってはなりません.
+   * @param title     本のタイトル. 100文字以下でかつ, nullであってはなりません.
    * @param author
    * @param publisher
    * @param price
@@ -34,6 +34,14 @@ public record Book(String id, String title, String author, String publisher, Int
 
     if (Long.parseLong(id) <= 0L || Long.parseLong(id) > 9999999999L) {
       throw new IllegalArgumentException("IDは1~9999999999でなくてはなりません.");
+    }
+
+    if (isNull(title)) {
+      throw new IllegalArgumentException("本タイトルがnullです.");
+    }
+
+    if (title.length() > 100) {
+      throw new IllegalArgumentException("本タイトルは100文字を超えてはなりません.");
     }
   }
 }
