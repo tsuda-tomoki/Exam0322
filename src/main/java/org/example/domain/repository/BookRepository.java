c1d62c124d6c0cfe29a1215e108569114360c825
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-10 16:31:22 +0900

    BookRepositoryの実装

--- src/main/java/org/example/domain/repository/BookRepository.java
+++ src/main/java/org/example/domain/repository/BookRepository.java
@@ -1,5 +1,17 @@
 package org.example.domain.repository;
 
+import java.util.List;
+import org.example.domain.Book;
+
+/**
+ * books テーブルにアクセスするためのリポジトリ.
+ */
 public interface BookRepository {
 
+  /**
+   * 全ての本情報を取得します.
+   *
+   * @return 本情報のリスト
+   */
+  List<Book> findAll();
 }
--- src/main/java/org/example/infrastructure/BookRepositoryImpl.java
+++ src/main/java/org/example/infrastructure/BookRepositoryImpl.java
@@ -1,7 +1,27 @@
 package org.example.infrastructure;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.domain.Book;
+import org.example.domain.entity.BookEntity;
 import org.example.domain.repository.BookRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * BookRepository の実装クラス.
+ * booksデータベースにアクセスします.
+ */
+@Repository
+@RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
 
+  private final BookMapper bookMapper;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public List<Book> findAll() {
+    return bookMapper.findAll().stream().map(BookEntity::convert).toList();
+  }
 }
