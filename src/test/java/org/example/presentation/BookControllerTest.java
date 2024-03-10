540687dda6602dbe4950f7bf67e19f61356c5d76
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-10 17:38:18 +0900

    全件検索のテストをコントローラーのテストに追加

--- src/test/java/org/example/presentation/BookControllerTest.java
+++ src/test/java/org/example/presentation/BookControllerTest.java
@@ -1,14 +1,23 @@
 package org.example.presentation;
 
+import java.util.List;
+import org.example.domain.Book;
+import org.example.presentation.response.AllBooksResponse;
+import org.example.presentation.response.BookResponse;
+import org.example.usecase.GetAllBooksUseCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.example.TestUtils.readFrom;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,6 +26,9 @@ class BookControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
+
+  @MockBean
+  private GetAllBooksUseCase sut;
 
   @BeforeEach
   void setUp() {
@@ -28,5 +40,20 @@ class BookControllerTest {
     // assert
     mockMvc.perform(get("/"))
         .andExpect(status().isOk());
+  }
+
+  @Test
+  void GETでエンドポイントにbooksが指定された場合全件検索が実行される() throws Exception {
+    // setup
+    List<Book> bookList = List.of(
+        new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+        new Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860)
+    );
+
+    doReturn(bookList).when(sut).findAll();
+
+    mockMvc.perform(get("/v1/books"))
+        .andExpect(status().isOk())
+        .andExpect(content().json(readFrom("test-json/AllBooks.json")));
   }
 }
