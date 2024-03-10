d4768157a19b8dbdca5229baf64b1cbc8315e08e
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-10 17:23:26 +0900

    GetAllBooksUseCaseの実装

--- src/main/java/org/example/usecase/GetAllBooksUseCase.java
+++ src/main/java/org/example/usecase/GetAllBooksUseCase.java
@@ -1,10 +1,23 @@
 package org.example.usecase;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.domain.Book;
+import org.example.domain.repository.BookRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * すべての本情報を取得するビジネスロジックを実装するユースケース.
+ */
 @Service
 @RequiredArgsConstructor
 public class GetAllBooksUseCase {
 
+  private BookRepository bookRepository;
+
+  @Transactional(readOnly = true)
+  public List<Book> findAll() {
+    return bookRepository.findAll();
+  }
 }
