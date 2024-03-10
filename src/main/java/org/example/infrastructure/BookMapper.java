b1cd75a747a9db468862e00206df0f775fb9f99d
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-10 11:46:49 +0900

    Mapperの実装

--- src/main/java/org/example/infrastructure/BookMapper.java
+++ src/main/java/org/example/infrastructure/BookMapper.java
@@ -1,5 +1,21 @@
 package org.example.infrastructure;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.example.domain.entity.BookEntity;
+
+/**
+ * 本情報を取得するマッパーインターフェイス.
+ */
+@Mapper
 public interface BookMapper {
 
+  /**
+   * 全ての本情報を取得します.
+   *
+   * @return 本情報のリスト
+   */
+  @Select("SELECT id, title, author, publisher, price FROM books")
+  List<BookEntity> findAll();
 }
