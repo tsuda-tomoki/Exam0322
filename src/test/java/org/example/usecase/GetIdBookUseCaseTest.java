df8c1cbae3dd39c7830a3bcd5a76932bf00f6563
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-11 23:29:33 +0900

    ユースケースのテストに指定されたIDが存在しないときに例外がスローされるテストを追加

--- src/test/java/org/example/usecase/GetIdBookUseCaseTest.java
+++ src/test/java/org/example/usecase/GetIdBookUseCaseTest.java
@@ -3,6 +3,7 @@ package org.example.usecase;
 import java.util.Optional;
 import org.example.domain.Book;
 import org.example.domain.repository.BookRepository;
+import org.example.usecase.exception.NotBookFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 class GetIdBookUseCaseTest {
@@ -40,5 +42,14 @@ class GetIdBookUseCaseTest {
 
     // assert
     assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void 指定されたIDが存在しないときに例外がスローされるとき() {
+    // setup
+    when(bookRepository.findById("999")).thenReturn(Optional.empty());
+
+    // assert
+    assertThatThrownBy(() -> sut.findById("999")).isInstanceOf(NotBookFoundException.class);
   }
 }
