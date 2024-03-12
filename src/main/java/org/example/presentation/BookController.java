f09b7f3352401f9e83069c40777040b5b6242559
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-12 20:42:33 +0900

    コントローラーにID検索を追加

--- src/main/java/org/example/presentation/BookController.java
+++ src/main/java/org/example/presentation/BookController.java
@@ -2,9 +2,12 @@ package org.example.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.example.presentation.response.AllBooksResponse;
+import org.example.presentation.response.BookResponse;
 import org.example.usecase.GetAllBooksUseCase;
+import org.example.usecase.GetIdBookUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
   private final GetAllBooksUseCase getAllBooksUseCase;
+  private final GetIdBookUseCase getIdBookUseCase;
 
   /**
    * ルートURLへのリクエストを処理します.
@@ -34,5 +38,17 @@ public class BookController {
   @ResponseStatus(HttpStatus.OK)
   public AllBooksResponse findAll() {
     return AllBooksResponse.format(getAllBooksUseCase.findAll());
+  }
+
+  /**
+   * 指定したIDの本情報を検索して返します.
+   *
+   * @param id 検索する本情報のID
+   * @return 指定されたIDの本情報を含むBookResponse
+   */
+  @GetMapping("/v1/books/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public BookResponse findById(@PathVariable String id) {
+    return BookResponse.format(getIdBookUseCase.findById(id));
   }
 }
