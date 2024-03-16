70daf838c63b226072da4890cd8d2bc323bda94c
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2024-03-16 12:17:07 +0900

    GlobalExceptionHandlerの仮実装、テストの実装

---
+++ src/test/java/org/example/common/GlobalExceptionHandlerTest.java
@@ -0,0 +1,64 @@
+package org.example.common;
+
+import org.example.domain.Book;
+import org.example.infrastructure.BookMapper;
+import org.example.infrastructure.BookRepositoryImpl;
+import org.example.usecase.GetAllBooksUseCase;
+import org.example.usecase.GetIdBookUseCase;
+import org.example.usecase.InsertBookUseCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.example.TestUtils.readFrom;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class GlobalExceptionHandlerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private BookMapper bookMapper;
+
+  @MockBean
+  private GetIdBookUseCase getIdBookUseCase;
+
+  @MockBean
+  private GetAllBooksUseCase getAllBooksUseCase;
+
+  @MockBean
+  private InsertBookUseCase insertBookUseCase;
+
+  @MockBean
+  private BookRepositoryImpl bookRepository;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void SqlFailExceptionがスローされた場合() throws Exception {
+    // setup
+    Book book = new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080);
+
+    doReturn(book).when(bookRepository).insert(book);
+
+    // assert & execute
+    mockMvc.perform(post("/v1/books")
+            .content(readFrom("test-json/postBookBad.json"))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
+  }
+}
--- src/test/java/org/example/presentation/BookControllerTest.java
+++ src/test/java/org/example/presentation/BookControllerTest.java
@@ -10,8 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,8 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 class BookControllerTest {
 
   @Autowired
